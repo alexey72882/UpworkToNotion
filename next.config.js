774380f58b7d4489f9aefd987b5ec5ee88e11b4e
Alexey Ilyin <alexey72882@gmail.com>
2025-10-26 13:49:53 -0600

    fix(vercel): ensure API routes are deployed

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  // DO NOT set output: 'export' (static export disables API routes)
+  reactStrictMode: true
+  // DO NOT add output: 'export' (it breaks API routes)
 };
 
 module.exports = nextConfig;
